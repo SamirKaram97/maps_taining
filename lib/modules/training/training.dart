a495f39f9a7a51d020cc3f714e02bd3176d0ea13
SamirKaram97 <samirkaramc97@gmail.com>
2022-06-22 15:07:27 +0200

    initial commit

---
+++ lib/modules/training/training.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TrainingPage extends StatefulWidget {
+  const TrainingPage({Key? key}) : super(key: key);
+
+  @override
+  State<TrainingPage> createState() => _TrainingPageState();
+}
+
+class _TrainingPageState extends State<TrainingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Training'),),
+      body: const Center(child: Text('Training'),),
+    );
+  }
+}
